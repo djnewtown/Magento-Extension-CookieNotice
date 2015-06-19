8b9ed31e379b4a8ca00a87415092e62b69b5dae0
Kathir Vel <vkathirvel@gmail.com>
2015-06-19 09:46:15 +0100

    0.2.2

--- app/code/community/Optimiseweb/CookieNotice/Model/System/Config/Source/Seconds.php
+++ app/code/community/Optimiseweb/CookieNotice/Model/System/Config/Source/Seconds.php
@@ -4,8 +4,8 @@
  * Optimiseweb CookieNotice Model System Config Source Seconds
  *
  * @package     Optimiseweb_CookieNotice
- * @author      Kathir Vel (sid@optimiseweb.co.uk)
- * @copyright   Copyright (c) 2014 Optimise Web
+ * @author      Kathir Vel (vkathirvel@gmail.com)
+ * @copyright   Copyright (c) 2015 Kathir Vel
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Optimiseweb_CookieNotice_Model_System_Config_Source_Seconds
